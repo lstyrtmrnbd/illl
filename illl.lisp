9308134fae6e0bcb85ba704cdafb0115a0203446
Mule Sknr <soymilk@airmail.cc>
2020-03-22 00:32:46 -0400

    Function for rule construction

--- illl.lisp
+++ illl.lisp
@@ -30,8 +30,17 @@
     result))
 
 (defun do-rewrite (string rules n)
+  "Does rewrite of string by rules n iterations"
   (labels ((recur (i string)
              (if (<= i 0)
                  string
                  (recur (1- i) (rewrite string rules)))))
     (recur n string)))
+
+(defun make-rules (rulelist)
+  "Turns a list of form ((char str) ...) into a rules hash-table"
+  (let ((result (make-hash-table :test 'equal)))
+    (dolist (rule rulelist)
+      (setf (gethash (car rule) result)
+            (car (cdr rule))))
+    result))
